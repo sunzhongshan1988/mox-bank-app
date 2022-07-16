52db46bb0cca4990b80a64b0e29594b0c9ff6c9f
sunzh <surface@pro.8>
2022-07-17 07:36:01 +0800

    some update

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
 
-  constructor() {}
+  selectTab: any;
+  activeMenu = false;
+
+  @ViewChild('tabs') tabs: IonTabs;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  setCurrentTab(event) {
+    console.log(event);
+    this.selectTab = this.tabs.getSelected();
+  }
+
+  onActiveMenu() {
+    this.activeMenu = !this.activeMenu;
+  }
 
 }
