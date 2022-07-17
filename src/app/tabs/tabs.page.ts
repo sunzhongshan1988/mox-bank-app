e3bcbe4a092133e778972906467e4eaba172ea57
sunzh <surface@pro.8>
2022-07-18 04:47:04 +0800

    fix: tab

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -22,24 +22,28 @@ export class TabsPage implements OnInit, AfterViewInit {
   }
   ngAfterViewInit() {
     console.log('ngAfterViewInit');
-    // Set the tab indicator position
-    this.tabs.select('tab1');
   }
 
-  setCurrentTab(event) {
-    console.log(event);
-    this.selectTab = this.tabs.getSelected();
-    const button = document.getElementById(`tab-button-${this.selectTab}`).getBoundingClientRect();
-    console.log(button);
-    if(this.selectTab === 'tab2') {
-      this.progrss.nativeElement.style.left = `${button.left + (button.width-48)/2}px`;
-    } else if (this.selectTab === 'tab3') {
-      this.progrss.nativeElement.style.left = `${button.left + (button.width-48)/2}px`;
-    } else if (this.selectTab === 'tab4') {
-      this.progrss.nativeElement.style.left = `${button.left + (button.width-48)/2}px`;
-    } else {
-      this.progrss.nativeElement.style.left = `${button.left + (button.width-48)/2}px`;
+  async setCurrentTab(event) {
+    try {
+      const button = document.getElementById(`tab-button-${event.tab}`).getBoundingClientRect();
+      console.log(button);
+      if(this.selectTab === 'tab2') {
+        this.progrss.nativeElement.style.left = `${button.left + (button.width-48)/2}px`;
+      } else if (this.selectTab === 'tab3') {
+        this.progrss.nativeElement.style.left = `${button.left + (button.width-48)/2}px`;
+      } else if (this.selectTab === 'tab4') {
+        this.progrss.nativeElement.style.left = `${button.left + (button.width-48)/2}px`;
+      } else {
+        this.progrss.nativeElement.style.left = `${button.left + (button.width-48)/2}px`;
+      }
+    } catch (error) {
+      console.log('Must be wait for the tab element after refresh the page.');
+      return setTimeout(() => {
+        this.setCurrentTab(event);
+      }, 100);
     }
+
   }
 
   onActiveMenu() {
